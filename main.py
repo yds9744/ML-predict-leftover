8f50bb1f1632aef11f18640778af27c6309b5c3c
jinsun <yds9744@naver.com>
2020-03-20 14:07:50 +0900

    Add main

---
+++ main.py
@@ -0,0 +1,24 @@
+import numpy as np
+from utils import initialize
+
+if __name__ == '__main__':
+    # Choose test ratio
+    test_ratio = 0.2
+
+    # Load dataset and model
+    test_data, train_data, model = initialize(test_ratio)
+    train_x, train_y = train_data
+
+    num_data, num_features = train_x.shape
+    print('# of Training data : ', num_data)
+
+    # Make model
+    model = model()
+
+    # TRAIN
+    model.train(train_x, train_y)
+
+    # EVALUATION
+    test_x, test_y = test_data
+    accuracy = model.test(test_x, test_y)
+    print(model, "test file accuracy:", accuracy)
