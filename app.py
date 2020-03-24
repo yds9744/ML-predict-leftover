bdcbe4ef8a4ace825301f58d5a9bb8bcf4c1d2b5
SangYeongLee <dltkddud4403@naver.com>
2020-03-24 23:57:39 +0900

    Add ajax function and dynamic html tag

--- app.py
+++ app.py
@@ -24,30 +24,31 @@ for idx, row in df.iterrows():
 def index():
 	return render_template('home.html')
 
-@app.route('/predict')
+@app.route('/predict', methods=['GET'])
 def predict():
 	return render_template('predict.html')
 
 @app.route('/predict', methods=['POST'])
 def predict_result():
 	req = request.form
-	
-	i=1
+
+	name = eval(req['name'])
+	supply = eval(req['supply'])
+
 	cate_data = [[0,0,0,0],[1,1,1,1],[2,2,0,2],[3,3,0,3],[4,4,0,4],[0,0,0,5]]
 	supp_data = []
-	while "menu"+str(i) in req:
-		cate_data.append([0]+encoded[req["menu"+str(i)]])
-		supp_data.append([int(req["supply"+str(i)])])
-		i+=1
+	for i in range(len(name)):
+		cate_data.append([0]+encoded[name[i]])
+		supp_data.append([int(supply[i])])
 	
 	one_hot_e = OneHotEncoder().fit_transform(cate_data).toarray()
-	
+
 	x = np.hstack( (one_hot_e[6:,:], supp_data) )
-	print(model.predict(x))
+	#print(model.predict(x))
 
 	ret = np.sum(model.predict(x))
 
-	return render_template('predict.html',price = round(ret,2))
+	return str(round(ret,2))
 
 @app.route('/auto', methods=['POST'])
 def auto():
